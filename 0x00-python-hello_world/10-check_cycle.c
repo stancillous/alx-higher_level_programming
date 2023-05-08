e44f6746d2c286f2cbbdb2e735a6dc36bcb8143c
stancillous <stancillousray@gmail.com>
2023-05-08 14:31:24 -0700

    check for cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,13 +11,15 @@ int check_cycle(listint_t *list)
 	listint_t *slow = list;
 	listint_t *fast = list;
 
+	if (!list || !list->next)
+		return (0);
+
 	while (slow && fast && fast->next->next)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
-
 		if (slow == fast)
 			return (1);
+		slow = slow->next;
+		fast = fast->next->next;
 	}
 	return (0);
 }
