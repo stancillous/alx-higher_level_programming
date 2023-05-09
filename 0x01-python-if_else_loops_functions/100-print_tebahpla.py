df0210172bc8b3642407f4a0eb89685b89181c66
stancillous <stancillousray@gmail.com>
2023-05-09 08:50:52 -0700

    print alphabet in reverse

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+a = 122
+state = "lower"
+output = ""
+
+while a >= 97:
+    if state == "lower":
+        output += chr(a)
+        state = "upper"
+    elif state == "upper":
+        output += chr(a-32)
+        state = "lower"
+    a = a - 1
+
+print(output, end="")
