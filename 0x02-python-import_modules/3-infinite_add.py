b2b502b4365a70bca49c45d74f799482b324d504
stancillous <stancillousray@gmail.com>
2023-05-11 05:48:35 -0700

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argvLen = (len(sys.argv)) - 1
     result = 0
 
-    for i in range(1, argvLen + 1):
-        result += int(sys.argv[i])
-    print(result)
+    for i, args in enumerate(sys.argv):
+        if i == 0:
+            continue
+        result += int(args)
+    print(f"{result:d}")
