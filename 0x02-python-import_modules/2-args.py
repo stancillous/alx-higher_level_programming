d7222ac3ed8d7c9ecafbfbaf37b35223e4546ef1
stancillous <stancillousray@gmail.com>
2023-05-11 03:15:54 -0700

    dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,14 @@
 if __name__ == "__main__":
     import sys
     argvLen = (len(sys.argv)) - 1
-    print("{} argument".format(argvLen)) if argvLen == 1 else print("{} arguments".format(argvLen) )
+
+    if argvLen == 1:
+        print("{} argument:".format(argvLen))
+    else:
+        if argvLen == 0:
+            print("{} arguments.".format(argvLen))
+        else:
+            print("{} arguments:".format(argvLen))
 
     for i in range(1, argvLen + 1):
         print("{}: {}".format(i, sys.argv[i]))
