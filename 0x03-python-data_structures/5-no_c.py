aef04e9e31febbc0c880f65b4ce4dd246b49dfda
stancillous <stancillousray@gmail.com>
2023-05-13 08:41:44 -0700

    remove character c from str

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if (my_string):
-        new_string = ""
-        for i in range(len(my_string)):
+    new_string = ""
+    for i in range(len(my_string)):
 
-            if my_string[i] in ["c", "C"]:
-                continue
-            new_string+= my_string[i]
-        return (new_string)
+        if my_string[i] in ["c", "C"]:
+            continue
+        new_string+= my_string[i]
+    return (new_string)
