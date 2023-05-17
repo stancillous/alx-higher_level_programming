9b9fa5accacea64254743517f48eadf78b1ceb19
stancillous <stancillousray@gmail.com>
2023-05-17 08:56:37 -0700

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,23 +5,21 @@ Return value of char
 
 
 def roman_value(char):
-    match char:
-        case 'I':
-            return 1
-        case 'V':
-            return 5
-        case 'X':
-            return 10
-        case 'L':
-            return 50
-        case 'C':
-            return 100
-        case 'D':
-            return 500
-        case 'M':
-            return 1000
-        case '_':
-            return -1
+    if char == 'I':
+        return 1
+    if char == 'V':
+        return 5
+    if char == 'X':
+        return 10
+    if char == 'L':
+        return 50
+    if char == 'C':
+        return 100
+    if char == 'D':
+        return 500
+    if char == 'M':
+        return 1000
+        return -1
 
 
 """
