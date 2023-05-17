0636cf43706ea8a3c21fd1e1aa87326f3383fefc
stancillous <stancillousray@gmail.com>
2023-05-17 02:53:24 -0700

    print sorted dict

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    new_dict = {}
+    for key in sorted(a_dictionary):
+        new_dict[key] = a_dictionary[key]
+    print(new_dict)
