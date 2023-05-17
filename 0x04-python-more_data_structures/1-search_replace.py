b7472f2c2876e39338897e1bf75e11f00aa9b3b2
stancillous <stancillousray@gmail.com>
2023-05-17 02:31:55 -0700

    search replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,6 +3,5 @@ def search_replace(my_list, search, replace):
     new_list = my_list[:]
     for i, j in enumerate(new_list):
         if j == search:
-            print(i, j)
             new_list[i] = replace
             return (new_list)
