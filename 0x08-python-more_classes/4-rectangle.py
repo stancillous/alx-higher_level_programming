6faf8880d95c22816b73dbacab5df5dff8f44e8e
stancillous <stancillousray@gmail.com>
2023-06-05 06:33:46 -0700

    add __repr__ method

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -106,10 +106,5 @@ class Rectangle:
 		return rect[:-1]
 	
 	def __repr__(self):
-            """method to return str rep of instance
-            Return:
-            instance str representation
-            """
-
 	    return "Rectangle({}, {})".format(self._width, self._height)
 	
