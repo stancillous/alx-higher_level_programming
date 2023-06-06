58c0a2f697ae48426dc7ccf79f541ab3993872a1
stancillous <stancillousray@gmail.com>
2023-06-06 09:30:55 -0700

    locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,6 @@
+#!/user/bin/python3
 class LockedClass:
+    """
+    prevent object instantiation of any attr
+    other thatn first_name"""
     __slots__ = ["first_name"]
-
-    def __init__(self):
-        pass
