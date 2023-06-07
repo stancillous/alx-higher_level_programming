02287727eaa63c6b6e02704a51ed9597825fc303
stancillous <stancillousray@gmail.com>
2023-06-07 13:28:26 -0700

    task solution

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""creating a new class"""
+
+
+class Square:
+    """
+    new Square class
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
