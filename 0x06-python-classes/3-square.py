2f0262459e98ab0831bf652a9766e2e0ee73d8e0
stancillous <stancillousray@gmail.com>
2023-06-07 13:37:13 -0700

    add class method

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -13,4 +13,9 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
     def area(self):
+        """
+        function to return area of square
+        Returns:
+        area of square
+        """
         return self.__size * self.__size
