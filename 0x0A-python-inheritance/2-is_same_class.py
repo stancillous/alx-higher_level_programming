49791ecfd3b2149638655136815b30ffde3b4bad
stancillous <stancillousray@gmail.com>
2023-06-12 02:13:42 -0700

    check if is class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 """
 func to check if obj is exactly of given class
-Args:
-    obj: object to be checked
-    a_class: class
-Returns: True or False
 """
 
 
 def is_same_class(obj, a_class):
+
+    """
+    func to check if obj is exactly of given class
+
+    Args:
+        obj: object to be checked
+        a_class: class
+
+    Returns: True or False
+    """
+
     if type(obj) is a_class:
         return True
     else:
