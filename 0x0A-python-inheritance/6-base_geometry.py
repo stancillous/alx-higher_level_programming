f7b0ee555ad07cdf3160eae668ea77685ecdf683
stancillous <stancillousray@gmail.com>
2023-06-12 01:00:25 -0700

    add func that raises exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""class Geometry"""
+
+
+class BaseGeometry:
+    def area(self):
+        """func that raises an exception"""
+        raise Exception("area() is not implemented")
