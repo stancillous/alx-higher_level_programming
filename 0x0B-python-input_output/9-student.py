29b0652c88687f24ccce340b8789a0aa6a7eb1b7
stancillous <stancillousray@gmail.com>
2023-06-13 09:49:04 -0700

    class student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+"""class Student"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """constructor method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """method that returns __dict__"""
+        return self.__dict__
