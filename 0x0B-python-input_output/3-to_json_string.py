462660288ca46f20d9d80f0960450dc6f72b61c0
stancillous <stancillousray@gmail.com>
2023-06-13 05:32:46 -0700

    object to json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+"""func to return json string of obj"""
+
+
+def to_json_string(my_obj):
+    """func to convert obj to json string
+    Args:
+        my_obj: obj to be converted
+    """
+    res = json.dumps(my_obj)
+    return res
