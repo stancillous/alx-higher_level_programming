b03aaadd060d2cb0fcdec6bde6cee3cba37f01ee
stancillous <stancillousray@gmail.com>
2023-06-13 04:58:32 -0700

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function to read a text file"""
+
+def read_file(filename=""):
+    """function to read a text file
+    Args:
+        filename: name of file to be read
+
+    """
+    with open(filename, encoding = "utf-8") as file:
+        print(file.read())
