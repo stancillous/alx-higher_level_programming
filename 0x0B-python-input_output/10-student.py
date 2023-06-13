7b52a83eb813f082597de16f30a6c93a12ee97a7
stancillous <stancillousray@gmail.com>
2023-06-13 10:12:18 -0700

    add method

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""class Student"""
+
+
+class Student:
+    """class Student"""
+    def __init__(self, first_name, last_name, age):
+        """constructor method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """method that returns__dict__"""
+        new_dict = {}
+        if attrs == None:
+            return self.__dict__
+        if type(attrs) == list:
+            for key in attrs:
+                if key in self.__dict__:
+                    new_dict[key] = self.__dict__[key]
+            return new_dict
