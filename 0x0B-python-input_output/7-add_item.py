792612b3b21d9df84262ee3f99415b3d1067e99d
stancillous <stancillousray@gmail.com>
2023-06-13 06:24:22 -0700

    new

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""importing sys"""
+import sys
+import os.path
+"""script that adds all arguments to a Python list,
+and then save them to a file"""
+
+saveToJsonFile = __import__('5-save_to_json_file').save_to_json_file
+loadFromJsonFile = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+args_list = []
+# if filename exsits
+if os.path.exists(filename):
+    if os.path.getsize(filename) == 0:
+        args_list = []
+    else:
+        args_list = loadFromJsonFile(filename)
+for i in range(1, len(sys.argv)):
+    args_list.append(sys.argv[i])
+saveToJsonFile(list, filename)
