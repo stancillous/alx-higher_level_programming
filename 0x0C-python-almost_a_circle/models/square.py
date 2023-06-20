fe7cb7795b014da578724ceb1fe49fca15135e37
stancillous <stancillousray@gmail.com>
2023-06-20 00:44:28 -0700

    added documentation for class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,11 +4,13 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """class Square"""
     def __init__(self, size, x=0, y=0, id=None):
         """constructor for square"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """method for str"""
         return ("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.height))
 
     @property
