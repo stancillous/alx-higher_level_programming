d1eae8e46bf4b002584b751d75023668cb1129dd
stancillous <stancillousray@gmail.com>
2023-07-19 05:48:53 -0700

    show users privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- show privileges for the users beloe
-SHOW GRANTS FOR user_0d_1@localhost
-SHOW GRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR 'user_0d_1@localhost';
+SHOW GRANTS FOR 'user_0d_2@localhost';
