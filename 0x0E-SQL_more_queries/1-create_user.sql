e1a1a74940ad1225c5a4052aa5243dcaed614152
stancillous <stancillousray@gmail.com>
2023-07-19 05:58:56 -0700

    create new use

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- create a MYSQL user
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd'
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
+CREATE USER IF NOT EXISTS 'user_0d_1@localhost' IDENTIFIED BY 'user_0d_1_pwd'
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1@localhost';
+FLUSH PRIVILEGES
