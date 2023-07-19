e08d654d69804fe9f2648e3755edf27bfc66a632
stancillous <stancillousray@gmail.com>
2023-07-19 07:15:50 -0700

    genre id by show

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,10 @@
+--script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.id NOT IN (
+  SELECT tv_show_genres.genre_id
+  FROM tv_shows
+  JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+  WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY tv_genres.name ASC;
