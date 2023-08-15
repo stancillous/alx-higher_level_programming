dde7aed8910213aa1e26aefce97e6dda2e2defca
stancillous <stancillousray@gmail.com>
2023-08-15 07:50:55 -0700

    get factorial of number

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+const num = Number(process.argv[2]);
+const fact = factorial(num);
+console.log(fact);
+
+function factorial (n) {
+  if (isNaN(n) || a <= 1) {
+    return 1;
+  }
+  return (n * factorial(n - 1));
+}
