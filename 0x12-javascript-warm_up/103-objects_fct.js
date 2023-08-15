573413e6d813ad9637ca3d81ae7bafbe307f0ad3
raymond <stancillousray@gmail.com>
2023-08-15 17:02:42 +0300

    manipulate object values

---
+++ 0x12-javascript-warm_up/103-objects_fct.js
@@ -0,0 +1,14 @@
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function incr () {
+  this.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
