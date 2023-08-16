9e14071e35188750c0b40780bae6db15c2e47061
stancillous <stancillousray@gmail.com>
2023-08-16 01:50:53 -0700

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,9 +5,6 @@ if (isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      process.stdout.write('X');
-    }
-    console.log('');
+    console.log('X'.repeat(size));
   }
 }
