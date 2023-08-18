0336e8df638f63d88a7fe8552d9834804dff0c44
raymond <stancillousray@gmail.com>
2023-08-18 18:16:48 +0300

    get states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""List all states in database"""
 
 if __name__ == "__main__":
-    import MySQLdb
     import sys
+    import MySQLdb
 
-    MY_HOST = 'localhost'
-    MY_USER = sys.argv[1]
-    MY_PASS = sys.argv[2]
-    MY_DB = sys.argv[3]
+    dbUser = sys.argv[1]
+    pswd = sys.argv[2]
+    dbName = sys.argv[3]
 
-    """Establish connection to DB"""
-    db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
+    db = MySQLdb.connect(host="localhost", user=dbUser, passwd=pswd, db=dbName)
+    query = "SELECT * FROM states ORDER BY states.id ASC"
 
-    """Create cursor object"""
     cur = db.cursor()
-
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
-
-    """Capture all rows returned by query"""
+    #execute our query
+    cur.execute(query)
+    #get result of previous execution
     rows = cur.fetchall()
-
-    for state in rows:
-        print(state)
+    #rows will be a list of tuples
+    for row in rows:
+        print(row)
