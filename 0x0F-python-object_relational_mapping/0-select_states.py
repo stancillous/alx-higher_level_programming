a9ab5f0ed70b045b4c9503a5ada924ebf851cb3f
raymond <stancillousray@gmail.com>
2023-08-18 18:22:56 +0300

    get states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""script that lists all states from a database"""
 if __name__ == "__main__":
     import sys
     import MySQLdb
@@ -8,14 +8,15 @@ if __name__ == "__main__":
     pswd = sys.argv[2]
     dbName = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", user=dbUser, passwd=pswd, db=dbName)
-    query = "SELECT * FROM states ORDER BY states.id ASC"
+    """connect w our database"""
+    db = MySQLdb.connect(host='localhost', user=dbUser, passwd=pswd, db=dbName)
 
     cur = db.cursor()
-    #execute our query
-    cur.execute(query)
-    #get result of previous execution
+    """execute our query"""
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    """get result of previous execution"""
     rows = cur.fetchall()
-    #rows will be a list of tuples
+    """rows will be a list of tuples"""
     for row in rows:
         print(row)
+
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -55,3 +55,36 @@ if __name__ == "__main__":
     pswd = sys.argv[2]
     dbName = sys.argv[3]
     listStates(user, pswd, dbName)
+
+
+
+
+
+
+    
+#!/usr/bin/python3
+"""List all states in database"""
+
+if __name__ == "__main__":
+    
+    import MySQLdb
+    import sys
+
+    MY_HOST = 'localhost'
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+
+    """Establish connection to DB"""
+    db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
+
+    """Create cursor object"""
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+
+    """Capture all rows returned by query"""
+    rows = cur.fetchall()
+
+    for state in rows:
+        print(state)
