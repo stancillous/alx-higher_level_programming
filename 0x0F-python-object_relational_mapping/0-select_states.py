13a03b1a0a5886a9cc3d62a709f363f0a3f3d94d
raymond <stancillousray@gmail.com>
2023-08-18 14:03:52 +0300

    tas 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-
-
-
-def listStates(user, pswd, dbName):
-    db = MySQLdb.connect(
-        host = "localhost",
-        user = user,
-        passwd = pswd,
-        db = dbName
-    )
-    cur = db.cursor()
-    query = "SELECT * FROM states ORDER BY id ASC"
-    cur.execute(query)
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-
+"""List all states in database"""
 
 if __name__ == "__main__":
-    import sys
     import MySQLdb
-    user = sys.argv[1]
-    pswd = sys.argv[2]
-    dbName = sys.argv[3]
-    listStates(user, pswd, dbName)
+    import sys
+
+    MY_HOST = 'localhost'
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+
+    """Establish connection to DB"""
+    db = MySQLdb.connect(host=MY_HOST, user=MY_USER, passwd=MY_PASS, db=MY_DB)
+
+    """Create cursor object"""
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+
+    """Capture all rows returned by query"""
+    rows = cur.fetchall()
+
+    for state in rows:
+        print(state)
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -26,3 +26,32 @@ if __name__ == "__main__":
     pswd = sys.argv[2]
     dbName = sys.argv[3]
     listFilteredStates(user, pswd, dbName)    
+
+
+
+
+
+
+#!/usr/bin/python3
+def listStates(user, pswd, dbName):
+    db = MySQLdb.connect(
+        host = "localhost",
+        user = user,
+        passwd = pswd,
+        db = dbName
+    )
+    cur = db.cursor()
+    query = "SELECT * FROM states ORDER BY id ASC"
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+    user = sys.argv[1]
+    pswd = sys.argv[2]
+    dbName = sys.argv[3]
+    listStates(user, pswd, dbName)
