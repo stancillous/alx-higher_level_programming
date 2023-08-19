2853f726f80ac08c314f2585db87484b3f129da4
raymond <stancillousray@gmail.com>
2023-08-19 22:14:43 +0300

    model states

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,41 +1,24 @@
 #!/usr/bin/python3
-"""Class State"""
+"""file that contains the class definition of a
+State and an instance Base = declarative_base()"""
 
-import sqlalchemy
-from sqlalchemy import create_engine, Column, Integer, UniqueConstraint, String
+import sys
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-# MY_USER = 'bot'
-# MY_PASSWD = 'TwoGreen1.'
-# MY_DB = 'hbtn_0e_4_usa'
-# MY_HOST = 'localhost'
-# MY_PORT = '3306'
-
-# connection_url = f"mysql://{MY_USER}:{MY_PASSWD}@{MY_HOST}:{MY_PORT}/{MY_DB}"
-
-# print()
-# print(connection_url)
-# print()
-
-# """Engine to connect to DB"""
-# engine = create_engine(connection_url)
-
-# """Actual connection to DB"""
-# connection = engine.connect()
-
-# result = connection.execute("SHOW DATABASES;")
-
-# for row in result:
-#     print(row)
-
-# connection.close()
+"""class State that inherits from Base"""
 
 
 class State(Base):
-    """Class State"""
-    __tablename__ = 'states'
+    """Represents a state for a MySQL database.
 
-    id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
-    name = Column(String(length=128), nullable=False)
+    __tablename__ :name of the MySQL table to store States.
+    id: state's id.
+    name: state's name.
+    """
+    __tablename__ = "states"
+    id = Column(Integer, autoincrement=True,
+                primary_key=True, unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
