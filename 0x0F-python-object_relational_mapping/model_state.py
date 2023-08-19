c5c7449f8983ed7b785d09c6709e3697c13e884d
raymond <stancillousray@gmail.com>
2023-08-19 22:12:28 +0300

    m

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,27 +1,41 @@
 #!/usr/bin/python3
-"""file that contains the class definition of a
-State and an instance Base = declarative_base()"""
+"""Class State"""
 
-import sys
-from sqlalchemy import create_engine, Column, Integer, String
+import sqlalchemy
+from sqlalchemy import create_engine, Column, Integer, UniqueConstraint, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-dbUser = sys.argv[1]
-pswd = sys.argv[2]
-dbName = sys.argv[3]
 
-"""class State that inherits from Base"""
+# MY_USER = 'bot'
+# MY_PASSWD = 'TwoGreen1.'
+# MY_DB = 'hbtn_0e_4_usa'
+# MY_HOST = 'localhost'
+# MY_PORT = '3306'
+
+# connection_url = f"mysql://{MY_USER}:{MY_PASSWD}@{MY_HOST}:{MY_PORT}/{MY_DB}"
+
+# print()
+# print(connection_url)
+# print()
+
+# """Engine to connect to DB"""
+# engine = create_engine(connection_url)
+
+# """Actual connection to DB"""
+# connection = engine.connect()
+
+# result = connection.execute("SHOW DATABASES;")
+
+# for row in result:
+#     print(row)
+
+# connection.close()
 
 
 class State(Base):
-    """Represents a state for a MySQL database.
+    """Class State"""
+    __tablename__ = 'states'
 
-    __tablename__ :name of the MySQL table to store States.
-    id: state's id.
-    name: state's name.
-    """
-    __tablename__ = "states"
-    id = Column("id", Integer, autoincrement=True,
-                primary_key=True, unique=True, nullable=False)
-    name = Column("name", String(128), nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
+    name = Column(String(length=128), nullable=False)
--- 0x0F-python-object_relational_mapping/new.py
+++ 0x0F-python-object_relational_mapping/new.py
@@ -1,21 +1,51 @@
-"""script that lists all cities from a database"""
+# """script that lists all cities from a database"""
 
-if __name__ == "__main__":
-    import MySQLdb
+# if __name__ == "__main__":
+#     import MySQLdb
 
-    dbUser = 'root'
-    pswd = 'kavulani@88'
-    dbName = 'mydb'
+#     dbUser = 'root'
+#     pswd = 'kavulani@88'
+#     dbName = 'mydb'
 
-    """make a connection w our db"""
-    db = MySQLdb.connect(host='localhost', user=dbUser, passwd=pswd, db=dbName)
+#     """make a connection w our db"""
+#     db = MySQLdb.connect(host='localhost', user=dbUser, passwd=pswd, db=dbName)
 
-    """cursor object"""
-    cur = db.cursor()
-    query = "SELECT * FROM orders ORDER BY orders.id ASC"
-    cur.execute(query)
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+#     """cursor object"""
+#     cur = db.cursor()
+#     query = "SELECT * FROM orders ORDER BY orders.id ASC"
+#     cur.execute(query)
+#     rows = cur.fetchall()
+#     for row in rows:
+#         print(row)
+#     cur.close()
+#     db.close()
+
+
+
+#!/usr/bin/python3
+"""file that contains the class definition of a
+State and an instance Base = declarative_base()"""
+
+import sys
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+dbUser = sys.argv[1]
+pswd = sys.argv[2]
+dbName = sys.argv[3]
+
+"""class State that inherits from Base"""
+
+
+class State(Base):
+    """Represents a state for a MySQL database.
+
+    __tablename__ :name of the MySQL table to store States.
+    id: state's id.
+    name: state's name.
+    """
+    __tablename__ = "states"
+    id = Column("id", Integer, autoincrement=True,
+                primary_key=True, unique=True, nullable=False)
+    name = Column("name", String(128), nullable=False)
