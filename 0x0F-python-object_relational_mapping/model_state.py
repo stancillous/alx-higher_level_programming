849aab26f33c518ab1ead7185e16420114f4fb93
raymond <stancillousray@gmail.com>
2023-08-19 22:09:33 +0300

    model states

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,33 +1,27 @@
 #!/usr/bin/python3
+"""file that contains the class definition of a
+State and an instance Base = declarative_base()"""
+
 import sys
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-# engine = create_engine('sql://username:password@host:port/database')
-# engine.connect()
 
 Base = declarative_base()
 dbUser = sys.argv[1]
 pswd = sys.argv[2]
 dbName = sys.argv[3]
 
+"""class State that inherits from Base"""
+
 
 class State(Base):
+    """Represents a state for a MySQL database.
+
+    __tablename__ :name of the MySQL table to store States.
+    id: state's id.
+    name: state's name.
+    """
     __tablename__ = "states"
     id = Column("id", Integer, autoincrement=True,
                 primary_key=True, unique=True, nullable=False)
-    name = Column("name", String, nullable=False)
-
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-
-
-engine = create_engine(f"mysql+mysqldb://{dbUser}:{pswd}@localhost/{dbName}")
-Base.metadata.create_all(bind=engine)
-
-Session = sessionmaker(bind=engine)
-session = Session()
-
-session.close()
+    name = Column("name", String(128), nullable=False)
