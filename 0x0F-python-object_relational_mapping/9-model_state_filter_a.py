19c2a34ca5f22c5642cae3437596d16b4a93989d
raymond <stancillousray@gmail.com>
2023-08-23 15:51:12 +0300

    use the LIKE operator

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,19 +12,14 @@ if __name__ == '__main__':
     dbPwd = sys.argv[2]
     dbName = sys.argv[3]
 
-    """Engine to connect to DB"""
     engine = create_engine(f"mysql://{dbUser}:{dbPwd}@localhost:3306/{dbName}")
 
-    """Session class for session objects"""
     Session = sessionmaker(bind=engine)
 
-    """Session with database started"""
     session = Session()
 
-    """Sample query"""
     results = session.query(State).\
         filter(State.name.like('%a%')).order_by(State.id.asc())
 
-    """Iterateing through results of query"""
     for state in results:
         print(f"{state.id}: {state.name}")
