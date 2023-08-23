13c3f9c7c14a8a3c5c98c52eb4bb24bf25baefcc
raymond <stancillousray@gmail.com>
2023-08-23 16:34:55 +0300

    cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+"""script that that prints all City objects
+from the database hbtn_0e_14_usa"""
 if __name__ == "__main__":
     import sys
     from model_state import Base, State
