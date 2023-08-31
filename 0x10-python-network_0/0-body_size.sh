b803f9c935d3e54a3b17eaa1dff77a3010443bc7
raymond <stancillousray@gmail.com>
2023-08-31 23:10:29 +0300

    get request

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # script to display size of body of a response
 # after using curl
-curl -s -i $1 | grep -i 'content-length' | awk '{print $2}'
+# curl -s -i $1 | grep -i 'content-length' | awk '{print $2}'
+curl -s "$1" | wc -c
