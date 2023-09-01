1504c8086dfc94a5894b5e1585f3aa7b9eb142b8
raymond <stancillousray@gmail.com>
2023-09-01 20:07:48 +0300

    100-task

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""script to list github commits of a repo"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits?per_page=10"
+    auth = {"Authorization": "Bearer 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"}
+    r = requests.get(url)
+    response = r.json()
+    for item in response:
+        print("{}: {}".format(item.get("sha"), item.get("committer")["login"]))
